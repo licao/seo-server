fc60b967e65e75d2736f3af3b0f66019a0dee8fa
NemoCao <huntformm@gmail.com>
2021-06-18 17:57:42 +0800

    更新配置项目 增加新能优化

--- server.js
+++ server.js
@@ -1,37 +1,28 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var history = require('connect-history-api-fallback');
-var SSR = require('./ssr.js');
-var listenPort = 8088;
-
-const staticFileMiddleware = express.static('dist');
-
-app.use(function(req, res, next){
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const config = require('config')
+const SSR = require('./ssr.js');
+const title = config.get('title')
+app.use((req, res, next)=>{
     // 生成本地访问链接
-    var requestUrl = 'https://support.oppo.com'+ req.url;
+    const requestUrl = config.get('server.targetUrl') + req.url;
     (async () => {
     try{
-        var results = await SSR(requestUrl);
+        const results = await SSR(requestUrl);
         res.send(results.html);
     }catch(e){
         console.log('ssr failed', e);
         res.status(500).send('Server error');
     }
     })();
-    // next();
 });
-
-
-// 先
-// app.use(staticFileMiddleware);
-
-// 如果资源没命中会继续、经过history rewirte后
-// app.use(history({
-//   disableDotRule: true,
-//   verbose: true
-// }));
-
-// 再次处理
-// app.use(staticFileMiddleware);
-server.listen(listenPort);
+const port = config.get('server.port') || 8080;
+server.listen(port,() => {
+    console.log('========== Support SEO Server (v1.0) started on port ' + port + ' ==========')
+    console.log('========== ' + title + ' ==========')
+    console.log('Listen Domain: ' + config.get('server.targetUrl'))
+    console.log('Cache File Dir: ' + config.get('server.cacheDir'))
+    console.log('Cache time: ' + (config.get('server.ttl')/3600) + ' h')
+    console.log('NODE_ENV: ' + process.env.NODE_ENV)
+});
--- ssr.js
+++ ssr.js
@@ -1,13 +1,40 @@
 const Cacheman = require('cacheman');
 const md5 = require('md5-node');
 const puppeteer = require('puppeteer');
+const config = require('config')
+const tmpDir = config.get('server.cacheDir') || './tmp'
+const ttl = config.get('server.ttl') || 60 * 60 * 3;
 const FilecCache = new Cacheman('htmls', {
     // 缓存3个小时
-    ttl: 60 * 60 * 3,
+    ttl: ttl,
     engine: 'file',
+    tmpDir: tmpDir
 });
-
-let browserWSEndpoint = null;
+const MAX_WSE = config.get('browser.size');  //启动几个浏览器
+let WSE_LIST = []; //存储browserWSEndpoint列表
+init();
+function init(){
+    (async () => {
+        for(var i=0;i<MAX_WSE;i++){
+            const browser = await puppeteer.launch({
+                dumpio:true,
+                headless: true,
+                ignoreHTTPSErrors: true,
+                args: [
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-setuid-sandbox',
+                '--no-first-run',
+                '--no-sandbox',
+                '--no-zygote',
+                '--single-process'
+            ]});
+            browserWSEndpoint = await browser.wsEndpoint();
+            WSE_LIST[i] = browserWSEndpoint;
+        }
+        console.log(WSE_LIST);
+    })();
+}
 
 module.exports = async function SSR(renderUrl){
 
@@ -18,6 +45,9 @@ module.exports = async function SSR(renderUrl){
     if(hitByCache){
         return hitByCache;
     }
+    let tmp = Math.floor(Math.random()* MAX_WSE);
+    let browserWSEndpoint = WSE_LIST[tmp];
+    // console.log('WSE SIT', tmp)
     if(browserWSEndpoint){
         try{
             browser = await puppeteer.connect({browserWSEndpoint});
@@ -28,13 +58,19 @@ module.exports = async function SSR(renderUrl){
     }
 
     if(!browserWSEndpoint){
+        console.log('error no browser');
         browser = await puppeteer.launch({ 
             dumpio:true,
             headless: true,
             ignoreHTTPSErrors: true,
             args: [
-                '--no-sandbox', 
-                '--disable-setuid-sandbox'
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-setuid-sandbox',
+                '--no-first-run',
+                '--no-sandbox',
+                '--no-zygote',
+                '--single-process'
             ]
         });
 
@@ -66,9 +102,10 @@ module.exports = async function SSR(renderUrl){
     });
     page.setViewport(viewConfig);
     await page.goto(renderUrl, {waitUntil: 'networkidle2'});
-    // await page.waitFor(10000);
+    await page.waitFor(5000);
     let html = await page.content();
-    html= html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '').replace(/<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/gi, '');
+    //去除script标签 style标签
+    html= html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '').replace(/<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/gi, '').replace(/<div id="oc-wrapper" style/g,'<div id="oc-wrapper" reStyle').replace(/<div class="cp-site-language-tip"/g,'<div class="cp-site-language-tip" style="display:none;').replace(/<div class="cp-cookie-tip"/g,'<div class="cp-cookie-tip" style="display:none;');
     await page.close();
     let results = {
         html
